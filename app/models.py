8c777a15769a5d294cb189e93aeec9ad638be15b
Srdjan Buric <srdjanburic05@gmail.com>
2021-02-08 12:40:20 +0100

    Model stanica: Umjesto atributa duzina sirina, jedan atribut tacka

--- app/models.py
+++ app/models.py
@@ -5,11 +5,12 @@ from django.shortcuts import reverse
 class Stanica(models.Model):
     naziv = models.CharField(max_length=200)
     opis = models.CharField(max_length=200)
-    duzina = models.CharField(max_length=100)
-    sirina = models.CharField(max_length=100)
+    tacka = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"Naziv: {self.naziv}, Opis: {self.opis}, Pozicija: ({self.duzina}, {self.sirina})"
+        return f"Naziv: {self.naziv}, Opis: {self.opis}, Pozicija: ({self.tacka})"
 
     def get_absolute_url(self):
         return reverse('stanice')
+
+
